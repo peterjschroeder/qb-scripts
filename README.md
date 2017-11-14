9234e6899979255fc211b0f49821660a2c644726
Peter J. Schroeder <peterjschroeder@gmail.com>
2017-11-13 19:52:12 -0500

    openLinks.py

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Userscripts for qutebrowser
 
 downloadAll.py	Downloads all links matching user inputed regex.
+openLinks.py	Opens all links in new tabs, with optional user inputed regex.
---
+++ openLinks.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+ 
+import re
+import requests
+import sys
+from bs4 import BeautifulSoup
+from os import environ
+from pathlib import Path
+from urllib.parse import urljoin
+
+def qexec(cmd):
+    with open(environ['QUTE_FIFO'], 'w') as f:
+        f.write(cmd)
+        return
+
+if __name__ == '__main__':
+    url = environ['QUTE_URL']
+    r = requests.get(url, headers={'User-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0'})
+    soup = BeautifulSoup(r.text, "lxml")
+    
+    if not (url.startswith("http")):
+        sys.exit(0)
+                
+    for tag in soup.findAll('a', href=True):
+        tag['href'] = urljoin(url, tag['href'])
+        
+        try:
+            if (len(sys.argv) < 2) or (re.search(sys.argv[1], tag['href'])):
+                qexec("open -t " + tag['href'] + "\n")
+        except:
+            continue
